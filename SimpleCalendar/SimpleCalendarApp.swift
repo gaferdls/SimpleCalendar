ec7db2b936cecbafcd34b7d9ea131e985124a6d4
geanpierre <gaferna3@asu.edu>
2025-08-08 19:44:58 -0700

    Initial Commit

---
+++ SimpleCalendar/SimpleCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCalendarApp.swift
+//  SimpleCalendar
+//
+//  Created by Geanpierre Fernandez on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimpleCalendarTests/SimpleCalendarTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCalendarTests.swift
+//  SimpleCalendarTests
+//
+//  Created by Geanpierre Fernandez on 8/8/25.
+//
+
+import Testing
+@testable import SimpleCalendar
+
+struct SimpleCalendarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
